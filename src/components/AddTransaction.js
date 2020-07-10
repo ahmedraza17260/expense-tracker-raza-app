2a22ac81e41f6567114f44e2e61d5de9d421b6ae
Ahmed Raza <ahmeraza17260@gmail.com>
2020-07-10 18:16:16 +0500

    changes in footer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -52,7 +52,12 @@ export const AddTransaction = () => {
         </div>
         <button className="btn">Add transaction</button>
         <div className="copyRight box_shadow">
-          <h4>Made by Ahmed Raza</h4>
+        <h4> Powered by Ahmed Raza </h4>
+            <a href="https://github.com/ahmedraza17260" rel="noreferrer">
+              {" "}
+              <h3> Copyright &copy; 2020 Ahmed Raza </h3>{" "}
+              <h3> All Right Reserved </h3>
+            </a>
         </div>
       </form>
     </>
